8bc4b4be12a0e57aadf8153a079f28f04ef94b8d
Gabriel <quontas@gmail.com>
2021-03-06 15:09:09 -0600

    DETR attempt

--- main.py
+++ main.py
@@ -1,197 +1,412 @@
-# %%
+# Copyright (c) Facebook, Inc. and its affiliates. All Rights Reserved
+import argparse
+import datetime
+import json
 import random
 import time
+from pathlib import Path
 
-import matplotlib.patches as patches
 import numpy as np
 import torch
-import torchvision
-from matplotlib import pyplot as plt
-from PIL import Image
-from torchvision.models.detection.faster_rcnn import FastRCNNPredictor
+from torch.utils.data import DataLoader, DistributedSampler
 
-import dataset
-import transforms as T
-import utils
-from coco_eval import CocoEvaluator
-from engine import WeirdLossException, evaluate, train_one_epoch
-
-TRAIN_BATCH_SIZE = 4
-TEST_BATCH_SIZE = 1
-
-PRINT_FREQUENCY = 50
-
-# %%
+import datasets
+import util.misc as utils
+from datasets import build_dataset, get_coco_api_from_dataset
+from engine import evaluate, train_one_epoch
+from models import build_model
+from datasets import transforms as T
 
 
-def get_detection_model(classes: int):
-    # load a model pre-trained on COCO
-    faster_rcnn = torchvision.models.detection.fasterrcnn_resnet50_fpn(pretrained=True)
+def get_args_parser():
+    parser = argparse.ArgumentParser("Set transformer detector", add_help=False)
+    parser.add_argument("--lr", default=1e-4, type=float)
+    parser.add_argument("--lr_backbone", default=1e-5, type=float)
+    parser.add_argument("--batch_size", default=2, type=int)
+    parser.add_argument("--weight_decay", default=1e-4, type=float)
+    parser.add_argument("--epochs", default=300, type=int)
+    parser.add_argument("--lr_drop", default=200, type=int)
+    parser.add_argument(
+        "--clip_max_norm", default=0.1, type=float, help="gradient clipping max norm"
+    )
 
-    # get number of input features for the classifier
-    in_features = faster_rcnn.roi_heads.box_predictor.cls_score.in_features
-    # replace the pre-trained head with a new one
-    faster_rcnn.roi_heads.box_predictor = FastRCNNPredictor(in_features, classes)
-    return faster_rcnn
+    # Model parameters
+    parser.add_argument(
+        "--frozen_weights",
+        type=str,
+        default=None,
+        help="Path to the pretrained model. If set, only the mask head will be trained",
+    )
+    # * Backbone
+    parser.add_argument(
+        "--backbone",
+        default="resnet50",
+        type=str,
+        help="Name of the convolutional backbone to use",
+    )
+    parser.add_argument(
+        "--dilation",
+        action="store_true",
+        help="If true, we replace stride with dilation in the last convolutional block (DC5)",
+    )
+    parser.add_argument(
+        "--position_embedding",
+        default="sine",
+        type=str,
+        choices=("sine", "learned"),
+        help="Type of positional embedding to use on top of the image features",
+    )
+
+    # * Transformer
+    parser.add_argument(
+        "--enc_layers",
+        default=6,
+        type=int,
+        help="Number of encoding layers in the transformer",
+    )
+    parser.add_argument(
+        "--dec_layers",
+        default=6,
+        type=int,
+        help="Number of decoding layers in the transformer",
+    )
+    parser.add_argument(
+        "--dim_feedforward",
+        default=2048,
+        type=int,
+        help="Intermediate size of the feedforward layers in the transformer blocks",
+    )
+    parser.add_argument(
+        "--hidden_dim",
+        default=256,
+        type=int,
+        help="Size of the embeddings (dimension of the transformer)",
+    )
+    parser.add_argument(
+        "--dropout", default=0.1, type=float, help="Dropout applied in the transformer"
+    )
+    parser.add_argument(
+        "--nheads",
+        default=8,
+        type=int,
+        help="Number of attention heads inside the transformer's attentions",
+    )
+    parser.add_argument(
+        "--num_queries", default=100, type=int, help="Number of query slots"
+    )
+    parser.add_argument("--pre_norm", action="store_true")
+
+    # * Segmentation
+    parser.add_argument(
+        "--masks",
+        action="store_true",
+        help="Train segmentation head if the flag is provided",
+    )
+
+    # Loss
+    parser.add_argument(
+        "--no_aux_loss",
+        dest="aux_loss",
+        action="store_false",
+        help="Disables auxiliary decoding losses (loss at each layer)",
+    )
+    # * Matcher
+    parser.add_argument(
+        "--set_cost_class",
+        default=1,
+        type=float,
+        help="Class coefficient in the matching cost",
+    )
+    parser.add_argument(
+        "--set_cost_bbox",
+        default=5,
+        type=float,
+        help="L1 box coefficient in the matching cost",
+    )
+    parser.add_argument(
+        "--set_cost_giou",
+        default=2,
+        type=float,
+        help="giou box coefficient in the matching cost",
+    )
+    # * Loss coefficients
+    parser.add_argument("--mask_loss_coef", default=1, type=float)
+    parser.add_argument("--dice_loss_coef", default=1, type=float)
+    parser.add_argument("--bbox_loss_coef", default=5, type=float)
+    parser.add_argument("--giou_loss_coef", default=2, type=float)
+    parser.add_argument(
+        "--eos_coef",
+        default=0.1,
+        type=float,
+        help="Relative classification weight of the no-object class",
+    )
+
+    # dataset parameters
+    parser.add_argument("--dataset_file", default="coco")
+    parser.add_argument("--coco_path", type=str)
+    parser.add_argument("--coco_panoptic_path", type=str)
+    parser.add_argument("--remove_difficult", action="store_true")
+
+    parser.add_argument(
+        "--output_dir", default="", help="path where to save, empty for no saving"
+    )
+    parser.add_argument(
+        "--device", default="cuda", help="device to use for training / testing"
+    )
+    parser.add_argument("--seed", default=42, type=int)
+    parser.add_argument("--resume", default="", help="resume from checkpoint")
+    parser.add_argument(
+        "--start_epoch", default=0, type=int, metavar="N", help="start epoch"
+    )
+    parser.add_argument("--eval", action="store_true")
+    parser.add_argument("--num_workers", default=2, type=int)
+
+    # distributed training parameters
+    parser.add_argument(
+        "--world_size", default=1, type=int, help="number of distributed processes"
+    )
+    parser.add_argument(
+        "--dist_url", default="env://", help="url used to set up distributed training"
+    )
+    return parser
 
 
-# %%
-# use our dataset and defined transformations
-transformations = T.Compose(
-    [
-        T.RandomHorizontalFlip(),
-        T.RandomGrayscale(),
-        T.ColorJitter(),
-        T.RandomResizedCrop(size=(256, 256)),
-        T.ToTensor(),
+def main(args):
+    utils.init_distributed_mode(args)
+    print("git:\n  {}\n".format(utils.get_sha()))
+
+    if args.frozen_weights is not None:
+        assert args.masks, "Frozen training is meant for segmentation only"
+    print(args)
+
+    device = torch.device(args.device)
+
+    # fix the seed for reproducibility
+    seed = args.seed + utils.get_rank()
+    torch.manual_seed(seed)
+    np.random.seed(seed)
+    random.seed(seed)
+
+    model, criterion, postprocessors = build_model(args)
+    model.to(device)
+
+    model_without_ddp = model
+    if args.distributed:
+        model = torch.nn.parallel.DistributedDataParallel(model, device_ids=[args.gpu])
+        model_without_ddp = model.module
+    n_parameters = sum(p.numel() for p in model.parameters() if p.requires_grad)
+    print("number of params:", n_parameters)
+
+    param_dicts = [
+        {
+            "params": [
+                p
+                for n, p in model_without_ddp.named_parameters()
+                if "backbone" not in n and p.requires_grad
+            ]
+        },
+        {
+            "params": [
+                p
+                for n, p in model_without_ddp.named_parameters()
+                if "backbone" in n and p.requires_grad
+            ],
+            "lr": args.lr_backbone,
+        },
     ]
-)
+    optimizer = torch.optim.AdamW(
+        param_dicts, lr=args.lr, weight_decay=args.weight_decay
+    )
+    lr_scheduler = torch.optim.lr_scheduler.StepLR(optimizer, args.lr_drop)
 
-test_transformations = T.Compose([T.ToTensor(),])
+    transformations = T.Compose(
+        [
+            T.RandomHorizontalFlip(),
+            T.RandomGrayscale(),
+            T.ColorJitter(),
+            T.RandomCrop(size=(256, 256)),
+            T.ToTensor(),
+        ]
+    )
 
-dataset_train = dataset.FRCNNFrameDataset(
-    "data/images",
-    "data/annotations/instances_default.json",
-    transforms=transformations,
-)
+    test_transformations = T.Compose(
+        [
+            T.ToTensor(),
+        ]
+    )
 
-dataset_test = dataset.FRCNNFrameDataset(
-    "data/images",
-    "data/annotations/instances_default.json",
-    transforms=test_transformations,
-)
+    dataset_train = datasets.coco.CocoDetection(
+        "data/images",
+        "data/annotations/instances_default.json",
+        transforms=transformations,
+        return_masks=False
+    )
 
-# split the dataset in train and test set
-torch.manual_seed(1)
-length = np.arange(len(dataset_train))
-# comment out this line if you don't want to permute the training and test set
-length = np.random.permutation(length)
+    dataset_test = datasets.coco.CocoDetection(
+        "data/images",
+        "data/annotations/instances_default.json",
+        transforms=test_transformations,
+        return_masks=False
+    )
 
-split_at = round(len(dataset_train) * 0.8)
-datasubset_train = torch.utils.data.Subset(dataset_train, length[:split_at])
-datasubset_test = torch.utils.data.Subset(dataset_test, length[split_at:])
+    # split the dataset in train and test set
+    torch.manual_seed(1)
+    length = np.arange(len(dataset_train))
+    # comment out this line if you don't want to permute the training and test set
+    length = np.random.permutation(length)
 
-# define training and validation data loaders
-data_loader_train = torch.utils.data.DataLoader(
-    datasubset_train,
-    batch_size=TRAIN_BATCH_SIZE,
-    shuffle=True,
-    num_workers=4,
-    collate_fn=utils.collate_fn,
-)
+    split_at = round(len(dataset_train) * 0.8)
+    datasubset_train = torch.utils.data.Subset(dataset_train, length[:split_at])
+    datasubset_val = torch.utils.data.Subset(dataset_test, length[split_at:])
 
-data_loader_test = torch.utils.data.DataLoader(
-    datasubset_test,
-    batch_size=TEST_BATCH_SIZE,
-    shuffle=False,
-    num_workers=4,
-    collate_fn=utils.collate_fn,
-)
+    if args.distributed:
+        sampler_train = DistributedSampler(datasubset_train)
+        sampler_val = DistributedSampler(datasubset_val, shuffle=False)
+    else:
+        sampler_train = torch.utils.data.RandomSampler(datasubset_train)
+        sampler_val = torch.utils.data.SequentialSampler(datasubset_val)
 
-# %%
-device = torch.device("cuda") if torch.cuda.is_available() else torch.device("cpu")
-print(f"Running on {device}")
+    batch_sampler_train = torch.utils.data.BatchSampler(
+        sampler_train, args.batch_size, drop_last=True
+    )
 
-# get the model using our helper function
-model = get_detection_model(len(dataset_train.coco.cats.keys()))
-# move model to the right device
-model.to(device)
+    data_loader_train = DataLoader(
+        datasubset_train,
+        batch_sampler=batch_sampler_train,
+        collate_fn=utils.collate_fn,
+        num_workers=args.num_workers,
+    )
+    data_loader_val = DataLoader(
+        datasubset_val,
+        args.batch_size,
+        sampler=sampler_val,
+        drop_last=False,
+        collate_fn=utils.collate_fn,
+        num_workers=args.num_workers,
+    )
 
-# construct an optimizer
-params = [p for p in model.parameters() if p.requires_grad]
-optimizer = torch.optim.SGD(params, lr=0.005, momentum=0.9, weight_decay=0.0005)
+    if args.dataset_file == "coco_panoptic":
+        # We also evaluate AP during panoptic training, on original coco DS
+        coco_val = datasets.coco.build("val", args)
+        base_ds = get_coco_api_from_dataset(coco_val)
+    else:
+        base_ds = get_coco_api_from_dataset(datasubset_val)
 
-# and a learning rate scheduler which decreases the learning rate by
-# 10x every 3 epochs
-lr_scheduler = torch.optim.lr_scheduler.StepLR(optimizer, step_size=3, gamma=0.1)
+    if args.frozen_weights is not None:
+        checkpoint = torch.load(args.frozen_weights, map_location="cpu")
+        model_without_ddp.detr.load_state_dict(checkpoint["model"])
 
-# %%
-def visualize_bounding_boxes(
-    image: Image, boxes: torch.Tensor, labels: torch.Tensor, scores, color="r"
-):
-    if scores is None:
-        scores = torch.ones(labels.shape)
-    _, ax = plt.subplots(1, figsize=(12, 8))
-    ax.imshow(image)
-    for box, label, score in zip(boxes, labels, scores):
-        if score.item() < 0.5:
-            continue
-        [min_x, min_y, max_x, max_y] = box.tolist()
-        width = max_x - min_x
-        height = max_y - min_y
-        rect = patches.Rectangle(
-            (min_x, min_y),
-            width,
-            height,
-            linewidth=1,
-            edgecolor=color,
-            facecolor="none",
-        )
-        ax.add_patch(rect)
-        ax.text(
-            min_x,
-            min_y,
-            f'{dataset_train.coco.cats[label.item()]["name"]} ({score.item()})',
-            c=color,
-        )
+    output_dir = Path(args.output_dir)
+    if args.resume:
+        if args.resume.startswith("https"):
+            checkpoint = torch.hub.load_state_dict_from_url(
+                args.resume, map_location="cpu", check_hash=True
+            )
+        else:
+            checkpoint = torch.load(args.resume, map_location="cpu")
+        model_without_ddp.load_state_dict(checkpoint["model"])
+        if (
+            not args.eval
+            and "optimizer" in checkpoint
+            and "lr_scheduler" in checkpoint
+            and "epoch" in checkpoint
+        ):
+            optimizer.load_state_dict(checkpoint["optimizer"])
+            lr_scheduler.load_state_dict(checkpoint["lr_scheduler"])
+            args.start_epoch = checkpoint["epoch"] + 1
 
-
-# %%
-num_epochs = 7
-
-for epoch in range(num_epochs):
-    # train for one epoch, printing every 10 iterations
-    try:
-        train_one_epoch(
+    if args.eval:
+        test_stats, coco_evaluator = evaluate(
             model,
-            optimizer,
+            criterion,
+            postprocessors,
+            data_loader_val,
+            base_ds,
+            device,
+            args.output_dir,
+        )
+        if args.output_dir:
+            utils.save_on_master(
+                coco_evaluator.coco_eval["bbox"].eval, output_dir / "eval.pth"
+            )
+        return
+
+    print("Start training")
+    start_time = time.time()
+    for epoch in range(args.start_epoch, args.epochs):
+        if args.distributed:
+            sampler_train.set_epoch(epoch)
+        train_stats = train_one_epoch(
+            model,
+            criterion,
             data_loader_train,
+            optimizer,
             device,
             epoch,
-            print_freq=PRINT_FREQUENCY,
+            args.clip_max_norm,
         )
-    except WeirdLossException as e:
+        lr_scheduler.step()
+        if args.output_dir:
+            checkpoint_paths = [output_dir / "checkpoint.pth"]
+            # extra checkpoint before LR drop and every 100 epochs
+            if (epoch + 1) % args.lr_drop == 0 or (epoch + 1) % 100 == 0:
+                checkpoint_paths.append(output_dir / f"checkpoint{epoch:04}.pth")
+            for checkpoint_path in checkpoint_paths:
+                utils.save_on_master(
+                    {
+                        "model": model_without_ddp.state_dict(),
+                        "optimizer": optimizer.state_dict(),
+                        "lr_scheduler": lr_scheduler.state_dict(),
+                        "epoch": epoch,
+                        "args": args,
+                    },
+                    checkpoint_path,
+                )
 
-        for img_tensor, target in zip(e.images, e.targets):
-            print(target)
-            im = Image.fromarray(
-                img_tensor.cpu().mul(255).permute(1, 2, 0).byte().numpy()
-            )
-            visualize_bounding_boxes(
-                im, target["boxes"], target["labels"], None, color="r"
-            )
-            plt.show()
-            plt.waitforbuttonpress()
-        raise Exception("Weird loss")
+        test_stats, coco_evaluator = evaluate(
+            model,
+            criterion,
+            postprocessors,
+            data_loader_val,
+            base_ds,
+            device,
+            args.output_dir,
+        )
 
-    # update the learning rate
-    lr_scheduler.step()
-    # evaluate on the test dataset
-    coco_evaluator: CocoEvaluator = evaluate(model, data_loader_test, device)
+        log_stats = {
+            **{f"train_{k}": v for k, v in train_stats.items()},
+            **{f"test_{k}": v for k, v in test_stats.items()},
+            "epoch": epoch,
+            "n_parameters": n_parameters,
+        }
 
-# %%
-idx = random.randint(0, len(dataset_test))
-img_tensor, test_target = dataset_test[idx]
-# put the model in evaluation mode
-model.eval()
-with torch.no_grad():
-    prediction = model([img_tensor.to(device)])
+        if args.output_dir and utils.is_main_process():
+            with (output_dir / "log.txt").open("a") as f:
+                f.write(json.dumps(log_stats) + "\n")
+
+            # for evaluation logs
+            if coco_evaluator is not None:
+                (output_dir / "eval").mkdir(exist_ok=True)
+                if "bbox" in coco_evaluator.coco_eval:
+                    filenames = ["latest.pth"]
+                    if epoch % 50 == 0:
+                        filenames.append(f"{epoch:03}.pth")
+                    for name in filenames:
+                        torch.save(
+                            coco_evaluator.coco_eval["bbox"].eval,
+                            output_dir / "eval" / name,
+                        )
+
+    total_time = time.time() - start_time
+    total_time_str = str(datetime.timedelta(seconds=int(total_time)))
+    print("Training time {}".format(total_time_str))
 
 
-im = Image.fromarray(img_tensor.mul(255).permute(1, 2, 0).byte().numpy())
-
-prediction_boxes = prediction[0]["boxes"]
-prediction_labels = prediction[0]["labels"]
-prediction_scores = prediction[0]["scores"]
-visualize_bounding_boxes(im, prediction_boxes, prediction_labels, prediction_scores)
-
-
-visualize_bounding_boxes(
-    im, test_target["boxes"], test_target["labels"], None, color="g"
-)
-# %%
-torch.save(model, "detector.pt")
-
-for i in range(5):
-    print("\a")
-    time.sleep(1)
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(
+        "DETR training and evaluation script", parents=[get_args_parser()]
+    )
+    args = parser.parse_args()
+    if args.output_dir:
+        Path(args.output_dir).mkdir(parents=True, exist_ok=True)
+    main(args)
